7d1d5a99d6377b47701fd9c6623c9cfc1c2825d5
moisesfdasilva <msesfernandes@gmail.com>
2023-05-17 16:29:09 -0300

    feat: desenvolvida a função search_by_date, em tech_news/analyzer/search_engine.py.

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 def search_by_title(title):
@@ -13,17 +14,26 @@ def search_by_title(title):
 
 
 def search_by_date(date):
-    # AAAA-mm-dd
-    news_list = search_news({"timestamp": {"$regex": date}})
-    date_and_url_list = []
+    date_ISO = ""
+
+    try:
+        date_format_ISO = '%Y-%m-%d'
+        date_ISO = datetime.strptime(date, date_format_ISO).date()
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    date_BR = format(date_ISO, '%d/%m/%Y')
+
+    news_list = search_news({"timestamp": {"$regex": date_BR}})
+
+    title_and_url_list = []
 
     for news in news_list:
-        news_truple = (news["title"], news["timestamp"])
-        date_and_url_list.append(news_truple)
+        news_truple = (news["title"], news["url"])
+        title_and_url_list.append(news_truple)
 
-    return date_and_url_list
+    return title_and_url_list
 
 
-# Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
